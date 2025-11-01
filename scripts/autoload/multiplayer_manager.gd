dfbb94f0ed39f61e28b60fc71620f2af65b1ca10
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2025-11-01 23:35:31 +0530

    Fix typos in multiplayer_manager.gd and add initial settings.json for Godot editor path

--- scripts/autoload/multiplayer_manager.gd
+++ scripts/autoload/multiplayer_manager.gd
@@ -1,1 +1,16 @@
 extends Node
+
+const SERVER_PORt = 8080
+
+func  create_server():
+    var enet_peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+    enet_peer.create_server(SERVER_PORt)
+    get_tree().get_multiplayer().multiplayer_peer = enet_peer
+    print("LOG: server created")
+
+func create_clinet(host_ip: String = "localhost", host_port = SERVER_PORt):
+    var enet_peer: ENetMultiplayerPeer =  ENetMultiplayerPeer.new()
+    enet_peer.create_client(host_ip,host_port)
+    get_tree().get_multiplayer().multiplayer_peer = enet_peer
+    print("LOG: clinet created")
+
